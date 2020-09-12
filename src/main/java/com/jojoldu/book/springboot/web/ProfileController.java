d76efa50db1050ee3fc81a1cb3c2ce9c32813326
SUY <simwinning@empal.com>
2020-09-12 19:32:59 +0900

    /profile rest API추가및 테스트코드작성

---
+++ src/main/java/com/jojoldu/book/springboot/web/ProfileController.java
@@ -0,0 +1,28 @@
+package com.jojoldu.book.springboot.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+@RequiredArgsConstructor
+@RestController
+public class ProfileController {
+    private final Environment env;
+
+    @GetMapping("/profile")
+    public String profile() {
+        List<String> profiles = Arrays.asList(env.getActiveProfiles()); //현재실행중인 Active Profile을 모두 가져온다. real, oauth, real-db ...
+        List<String> realProfiles = Arrays.asList("real", "real1", "real2");
+        String defaultProfile = profiles.isEmpty()? "default" : profiles.get(0);
+
+        return profiles.stream()
+                .filter(realProfiles::contains)
+                .findAny()
+                .orElse(defaultProfile);
+    }
+}
